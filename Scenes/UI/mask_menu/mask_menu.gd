72f2550f22751671bd82d8e9840d3050f4fe64d4
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-10-17 11:24:12 -0700

    Add returning runes to list

--- Scenes/UI/mask_menu/mask_menu.gd
+++ Scenes/UI/mask_menu/mask_menu.gd
@@ -10,23 +10,26 @@ func populate_runes(runes: Array[RuneData]) -> void:
 	for rune_data in runes:
 		if rune_entries.has(rune_data):
 			continue
-		var rune: RuneEntry = RUNE_ENTRY.instantiate()
-		rune.verbosity = RuneEntry.Verbosity.IconLabel
-		rune.populate_data(rune_data)
-		rune.tree_exiting.connect(handle_rune_tree_exiting.bind(rune_data))
-		
-		var drag_drop_comp = DragDropControlComponent.new()
-		rune.add_child(drag_drop_comp)
-		
-		var delete_slot_comp = DeleteSlotOnEmptyComponent.new()
-		rune.add_child(delete_slot_comp)
-		
-		rune_entries[rune_data] = rune
-		rune_container.add_child(rune)
-		rune_container.move_child(rune, 0)
-		
-		drag_drop_comp.data_slot.rune_data = rune_data
-		delete_slot_comp.drag_drop_comp = drag_drop_comp
+		create_rune(rune_data)
+
+func create_rune(rune_data: RuneData) -> void:
+	var rune: RuneEntry = RUNE_ENTRY.instantiate()
+	rune.verbosity = RuneEntry.Verbosity.IconLabel
+	rune.populate_data(rune_data)
+	rune.tree_exiting.connect(handle_rune_tree_exiting.bind(rune_data))
+	
+	var drag_drop_comp = DragDropControlComponent.new()
+	rune.add_child(drag_drop_comp)
+	
+	var delete_slot_comp = DeleteSlotOnEmptyComponent.new()
+	rune.add_child(delete_slot_comp)
+	
+	rune_entries[rune_data] = rune
+	rune_container.add_child(rune)
+	rune_container.move_child(rune, 0)
+	
+	drag_drop_comp.data_slot.rune_data = rune_data
+	delete_slot_comp.drag_drop_comp = drag_drop_comp
 
 func handle_rune_tree_exiting(rune_data: RuneData) -> void:
 	rune_entries.erase(rune_data)
