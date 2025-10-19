fb788e781d223a67f1462a8463dbed4a83e3808f
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-10-19 07:26:27 -0700

    Add player invincibility on hit

--- Scenes/Components/Combat/Health/health.gd
+++ Scenes/Components/Combat/Health/health.gd
@@ -7,6 +7,7 @@ var current_health: float
 var max_health: float
 var is_dead := false
 var is_invincible := false # Just in case we will need to stop incoming damage
+var invincible_timer: float = 0
 
 signal died
 
@@ -14,12 +15,18 @@ func _ready() -> void:
 	max_health = player_stats.max_health;
 	current_health = max_health;
 
+func _physics_process(delta: float) -> void:
+	if invincible_timer > 0:
+		invincible_timer -= delta
+
 # This function is using DAMAGE type by default. If healing needed call with second param equal to HEALING
 func update_health(value: float, type: UpdateType = UpdateType.DAMAGE):
 	match type: 
 		UpdateType.DAMAGE:
-			if not is_invincible:
+			if not is_invincible and invincible_timer <= 0:
 				current_health = max(current_health - value, 0)
+				if player_stats.health_damage_invincibility_time > 0:
+					invincible_timer = player_stats.health_damage_invincibility_time
 				print("Damage received!")
 				Callable(check_death).call_deferred()
 		UpdateType.HEALING:
