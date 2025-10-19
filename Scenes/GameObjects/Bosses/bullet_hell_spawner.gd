5b2562042e4d2b33daedde24e46bd384820074f8
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2025-10-19 07:26:19 -0700

    Add double stomp attack

--- Scenes/GameObjects/Bosses/bullet_hell_spawner.gd
+++ Scenes/GameObjects/Bosses/bullet_hell_spawner.gd
@@ -8,7 +8,10 @@ func _ready() -> void:
 
 func fire(pattern: BulletHellSpawnPattern) -> void:
 	var bullet_arc_length = (pattern.arc_length / (pattern.amount - 1))
-	for i in pattern.amount:
-		var angle = bullet_arc_length * i + pattern.arc_offset
-		bullet_container.fire_bullet(boss.global_position, angle, pattern.bullet_data)
-		
+	for j in pattern.interations:
+		for i in pattern.amount:
+			var angle = bullet_arc_length * i + pattern.arc_offset
+			bullet_container.fire_bullet(boss.global_position, angle, pattern.bullet_data)
+		if j == pattern.interations - 1:
+			continue
+		await get_tree().create_timer(pattern.iteration_cooldown).timeout
